175072cc50d02e43a8492a0573dcce62d81d22db
HC-29 <mythic2357@gmail.com>
2026-01-23 20:10:42 -0600

    Initial Tailwind initialization test

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'vector-cyan': '#00f2ff',
+        'vector-dark': '#0b1026',
+      },
+    },
+  },
+  plugins: [],
+}
